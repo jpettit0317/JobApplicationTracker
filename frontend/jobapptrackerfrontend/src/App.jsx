04e2b5aca62ef8feabc29b33cf2eec50100b1a13
Jack Pettit <jpettit0317@gmail.com>
2023-03-20 18:02:16 -0500

    Removed unused NavBar import. Converted to a jsx file. Needed to resolve an issue when importing LoginPage. Moved App.test.js to the root of the project.

--- frontend/jobapptrackerfrontend/src/App.jsx
+++ frontend/jobapptrackerfrontend/src/App.jsx
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './components/navbar/NavBar';
 import { LoginPage } from './components/Login/LoginPage';
 
 function App() {
