bc872f9b308c5e0ac5be3896036db1239a11e3a3
Jack Pettit <jpettit0317@gmail.com>
2023-03-20 18:05:24 -0500

    Added loginconstants.js. These are errors used by Login and Register.

--- frontend/jobapptrackerfrontend/src/constants/login-constants.js
+++ frontend/jobapptrackerfrontend/src/constants/login-constants.js
@@ -3,3 +3,4 @@ export const passwordNotInRange = "Your password must be 8-20 characters long, "
 "and must not contain spaces, special characters, or emoji.";
 
 export const emailAddressIsEmpty = "Email address is empty.";
+export const passwordsMismatch = "Passwords don't match";
