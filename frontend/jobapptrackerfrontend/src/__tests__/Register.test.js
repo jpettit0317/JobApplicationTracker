df0db0e56a619b4fa5577f04f112bfc97c7ad56d
Jack Pettit <jpettit0317@gmail.com>
2023-03-21 14:40:48 -0500

    Refactored values into an object. Added a test for emojis.

--- frontend/jobapptrackerfrontend/src/__tests__/Register.test.js
+++ frontend/jobapptrackerfrontend/src/__tests__/Register.test.js
@@ -5,8 +5,14 @@ import { passwordNotInRange,
 } from "../constants/login-constants";
 
 describe('Register tests', () => {
-    const validUserEmail = "user@email.com";
-    const validPassword = "validUser23;";
+    const login = {
+        validUserEmail: "user@email.com",
+        validPassword: "validUser23",
+        emptyUserEmail: "",
+        emptyPassword: "",
+        emojiPassword: "Hello World! 🤣"
+
+    };
 
     const assertErrorsAreEqual = (actual, expected) => {
         expect(actual.getEmailError()).toBe(expected.getEmailError());
@@ -16,7 +22,8 @@ describe('Register tests', () => {
 
     describe('getError tests', () => {
         test('when passed in empty values, should return errors for all fields', () => {
-            const sut = new Register("", "", "");
+            const sut = new Register(login.emptyUserEmail,
+                 login.emptyPassword, login.emptyPassword);
             const expectedValue = new RegisterErrors(emailAddressIsEmpty,
                  passwordNotInRange, passwordNotInRange);
             const actualValue = sut.getErrors();
@@ -25,9 +32,20 @@ describe('Register tests', () => {
         });
 
         test('when passed in valid values, should not return any errors', () => {
-            const sut = new Register(validUserEmail, validPassword, validPassword);
+            const sut = new Register(login.validUserEmail, login.validPassword,
+                login.validPassword);
 
             const expectedValue = new RegisterErrors("", "", "");
+            const actualValue = sut.getErrors();
+
+            assertErrorsAreEqual(actualValue, expectedValue);
+        });
+
+        test('when passed in a valid username and an emoji password, should return errors', () => {
+            const sut = new Register(login.expectedValue,
+                login.emojiPassword, login.emojiPassword);
+
+            const expectedValue = new RegisterErrors("", passwordNotInRange, passwordNotInRange);
             const actualValue = sut.getErrors();
 
             assertErrorsAreEqual(actualValue, expectedValue);
