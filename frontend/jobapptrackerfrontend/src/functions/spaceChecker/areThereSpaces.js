e0da4b619fbd6be06731c8e0fcce7c4604e8cc08
Jack Pettit <jpettit0317@gmail.com>
2023-03-22 10:34:35 -0500

    Implemented areThereSpaces and its tests.

--- frontend/jobapptrackerfrontend/src/functions/spaceChecker/areThereSpaces.js
+++ frontend/jobapptrackerfrontend/src/functions/spaceChecker/areThereSpaces.js
@@ -5,5 +5,9 @@ areThereSpaces(input: String) : Bool
 checks if there is a space in input.
 */
 export const areThereSpaces = (input) => {
+    if (input === "") {
+        return false;
+    }
 
+    return /\s/.test(input);
 }
