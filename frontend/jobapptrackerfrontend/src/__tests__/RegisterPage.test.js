915d32295325db8190b97d89849218331677b8f4
Jack Pettit <jpettit0317@gmail.com>
2023-03-22 09:46:20 -0500

    Added test that tests if there are errors when pressing submit on RegisterPage when there is nothing in all fields.

--- frontend/jobapptrackerfrontend/src/__tests__/RegisterPage.test.js
+++ frontend/jobapptrackerfrontend/src/__tests__/RegisterPage.test.js
@@ -1,4 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 import { RegisterPage } from "../components/Register/RegisterPage";
 
 
@@ -48,6 +50,25 @@ describe('Register Page UI tests', () => {
             expect(emailAddressHelper).not.toBeInTheDocument();
             expect(passwordHelperText).not.toBeInTheDocument();
             expect(confirmPasswordHelper).not.toBeInTheDocument();
+        });
+    });
+
+    describe('changing value tests', () => {
+        test('when username, password, and confirm password is empty' +
+        ' there should be errors on all three fields', async () => {
+            render(<RegisterPage />);
+
+            act(() => {
+                userEvent.click(screen.getByText(/submit/i));
+            });
+
+            await waitFor(() => {
+                expect(screen.getByText(/Email address is empty./i)).toBeInTheDocument();
+                expect(screen.queryByTestId(testIds.emailAddressHelperText)).toBeInTheDocument();
+                expect(screen.queryByTestId(testIds.passwordHelperText)).toBeInTheDocument();
+                expect(screen.queryByTestId(testIds.confirmPasswordHelperText)).toBeInTheDocument();
+            });
+
         });
     });
 });
