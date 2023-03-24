bd684e15265166d133698f3494685d82b60206ca
Jack Pettit <jpettit0317@gmail.com>
2023-03-24 10:36:12 -0500

    Added routes and an error page to the project.

--- frontend/jobapptrackerfrontend/src/App.jsx
+++ frontend/jobapptrackerfrontend/src/App.jsx
@@ -2,11 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { LoginPage } from './components/Login/LoginPage';
+import { RegisterPage } from './components/Register/RegisterPage';
+import { 
+  createBrowserRouter,
+  RouterProvider 
+} from 'react-router-dom';
+import { ErrorPage } from './components/errorpages/ErrorPage';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LoginPage />,
+      errorElement: <ErrorPage />
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+      errorElement: <ErrorPage />
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+      errorElement: <ErrorPage />
+    }
+  ]);
+
   return (
     <div className="App">
-      <LoginPage />
+      <RouterProvider router={router} />
     </div>
   );
 }
