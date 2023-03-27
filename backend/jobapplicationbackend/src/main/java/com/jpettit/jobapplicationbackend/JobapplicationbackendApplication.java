74784cbc7475a2cd6afaf876b63666f396b3abf5
Jack Pettit <jpettit0317@gmail.com>
2023-03-27 10:02:33 -0500

    Added Spring Boot project.

---
+++ backend/jobapplicationbackend/src/main/java/com/jpettit/jobapplicationbackend/JobapplicationbackendApplication.java
@@ -0,0 +1,13 @@
+package com.jpettit.jobapplicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobapplicationbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobapplicationbackendApplication.class, args);
+	}
+
+}
---
+++ backend/jobapplicationbackend/src/test/java/com/jpettit/jobapplicationbackend/JobapplicationbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpettit.jobapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobapplicationbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
