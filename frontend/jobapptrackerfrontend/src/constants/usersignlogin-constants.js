608a16c45e51a8750035b1c0417c87a049f0f8b1
Jack Pettit <jpettit0317@gmail.com>
2023-04-12 16:55:24 -0500

    Removed old JavaScript frontend. Transferring it to TypeScript.

--- frontend/jobapptrackerfrontend/src/constants/usersignlogin-constants.js
+++
@@ -1,6 +0,0 @@
-export const passwordNotInRange = "Your password must be 8-20 characters long, " + 
-"contain letters and numbers, " + 
-"and must not contain spaces, special characters, or emoji.";
-
-export const emailAddressIsEmpty = "Email address is empty.";
-export const passwordsMismatch = "Passwords don't match";
