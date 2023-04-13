fd626924f25761fa188ca09899b20d637db4dc39
Jack Pettit <jpettit0317@gmail.com>
2023-04-13 11:46:28 -0500

    Added getElementByText. Used to get an element by text.

--- frontend/jobapptrackerfrontend/src/__test__/helperfunctions/htmlelements/getElement.tsx
+++ frontend/jobapptrackerfrontend/src/__test__/helperfunctions/htmlelements/getElement.tsx
@@ -3,3 +3,7 @@ import { screen } from '@testing-library/react';
 export const getElement = (id: string): HTMLElement | null  => {
     return screen.queryByTestId(id);
 }
+
+export const getElementByText = (title: string): HTMLElement | null => {
+    return screen.queryByText(title);
+}
