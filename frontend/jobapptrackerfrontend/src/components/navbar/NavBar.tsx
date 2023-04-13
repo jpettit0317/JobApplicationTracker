83f3fef6568168865dab96cd149dc115c5e3978d
Jack Pettit <jpettit0317@gmail.com>
2023-04-13 11:56:22 -0500

    Added NavBarTestIds to the project.

--- frontend/jobapptrackerfrontend/src/components/navbar/NavBar.tsx
+++ frontend/jobapptrackerfrontend/src/components/navbar/NavBar.tsx
@@ -1,6 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { NavBarTestIds } from './testIds/NavBarTestIds';
 /*
 NavBar
 
@@ -12,9 +12,9 @@ onSearchButtonPressed(searchTerm: string) -> Void
 export const NavBar = () => {
     return (
         <div>
-          <Navbar bg="light" expand="lg" data-testid="navbar">
+          <Navbar bg="light" expand="lg" data-testid={NavBarTestIds.navBar}>
             <Container>
-              <Navbar.Brand href="#home" data-testid="navbarbrand">
+              <Navbar.Brand href="#home" data-testid={NavBarTestIds.navBarBrand}>
                 Job Application Tracker</Navbar.Brand>
             </Container>
           </Navbar>
