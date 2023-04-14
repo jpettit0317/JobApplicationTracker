9ef22e0f86e45a24b6cb074aceb9a994dcec077d
Jack Pettit <jpettit0317@gmail.com>
2023-04-14 18:13:17 -0500

    Added minimum and maximum password legnth.

--- frontend/jobapptrackerfrontend/src/constants/LoginSignUpConstants.ts
+++ frontend/jobapptrackerfrontend/src/constants/LoginSignUpConstants.ts
@@ -2,5 +2,10 @@ export class LoginSignUpConstants {
     static readonly emailAddressIsEmpty = "Email address is empty.";
     static readonly passwordContainsSpecialCharacters = `Your password must be 8-20 characters long,
     contain letters and numbers, and must not contain spaces, or emoji.`;
+    static readonly firstnameIsEmpty = "Firstname is empty.";
+    static readonly lastnameIsEmpty = "Lastname is empty.";
     static readonly emailContainsSpecialCharacters = "Your email contains spaces or emojis.";
+    static readonly passwordsDoNotMatch = "Passwords don't match.";
+    static readonly minPasswordLength = 8;
+    static readonly maxPasswordLength = 20;
 }
