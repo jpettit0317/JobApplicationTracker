0545692a55e7005feeb54bb4714426b8ccef4bcc
Jack Pettit <jpettit0317@gmail.com>
2023-04-14 18:14:20 -0500

    Implemented errors for SignUpConfirmPasswordField.

--- frontend/jobapptrackerfrontend/src/functions/getSignUpErrors.ts
+++ frontend/jobapptrackerfrontend/src/functions/getSignUpErrors.ts
@@ -1,6 +1,10 @@
 import { SignUpErrorBuilder } from "../model/builders/SignUpErrorBuilder";
 import { SignUpConfirmPassword } from "../model/interfaces/signup/SignUp";
 import { SignUpErrors } from "../model/interfaces/signup/SignUpErrors";
+import { LoginSignUpConstants } from "../constants/LoginSignUpConstants";
+import { areThereEmojis } from "./areThereEmojis";
+import { areThereSpaces } from "./areThereSpaces";
+import { isInputInRange } from "./isInputInRange";
 
 export const getSignUpErrors = (signUp: SignUpConfirmPassword): SignUpErrors => {
     const emailState = getEmailError(signUp.email);
@@ -32,36 +36,97 @@ export const getSignUpErrors = (signUp: SignUpConfirmPassword): SignUpErrors => 
 
 const getEmailError = (email: string):
  {emailError: string, isEmailInErrorState: boolean} => {
+    let emailError: string = "";
+    let isEmailInErrorState: boolean = false;
+
+    if (email === "") {
+        emailError = LoginSignUpConstants.emailAddressIsEmpty;
+        isEmailInErrorState = true;
+    } else if (areThereEmojis(email) || areThereSpaces(email)) {
+        emailError = LoginSignUpConstants.emailContainsSpecialCharacters;
+        isEmailInErrorState = false;
+    } 
     return {
-        emailError: "",
-        isEmailInErrorState: false
+        emailError: emailError,
+        isEmailInErrorState: isEmailInErrorState
     };
 }
 
 const getFirstnameError = (firstname: string):
  {firstnameError: string, isFirstNameInErrorState: boolean} => {
+    let firstnameError: string = "";
+    let isFirstNameInErrorState: boolean = false;
+
+    if (firstname === "") {
+        firstnameError = LoginSignUpConstants.firstnameIsEmpty;
+        isFirstNameInErrorState = true;
+    }
     return {
-        firstnameError: "",
-        isFirstNameInErrorState: false
+        firstnameError: firstnameError,
+        isFirstNameInErrorState: isFirstNameInErrorState
     };
 }
 
 const getLastnameError = (lastname: string):
  {lastnameError: string, isInErrorState: boolean} => {
-    return {
-        lastnameError: "",
-        isInErrorState: false
+    let lastnameError: string = "";
+    let isInErrorState: boolean = false;
+
+    if (lastname === "") {
+        lastnameError = LoginSignUpConstants.lastnameIsEmpty;
+        isInErrorState = true;
     }
+
+    return {
+        lastnameError: lastnameError,
+        isInErrorState: isInErrorState
+    };
 }
 
 const getPasswordErrors = (password: string, confirmPassword: string):
  {passwordError: string, isPasswordInErrorState: boolean,
     confirmPasswordError: string, isConfirmPasswordInErrorState: boolean} => {
+    let passwordError: string = "";
+    let confirmPasswordError: string = "";
+    let isPasswordInErrorState: boolean = false;
+    let isConfirmPasswordInErrorState: boolean = false;
+    const passwordRanges = {
+        min: LoginSignUpConstants.minPasswordLength,
+        max: LoginSignUpConstants.maxPasswordLength
+    };
+
+    if (password === "") {
+        passwordError = LoginSignUpConstants.passwordContainsSpecialCharacters;
+        isPasswordInErrorState = true;
+    }
+
+    if (confirmPassword === "") {
+        confirmPasswordError = LoginSignUpConstants.passwordContainsSpecialCharacters;
+        isConfirmPasswordInErrorState = true;
+    }
+
+    if (!isInputInRange(password, passwordRanges.min, passwordRanges.max)) {
+        passwordError = LoginSignUpConstants.passwordContainsSpecialCharacters;
+        isPasswordInErrorState = true;
+    }
+
+    if (!isInputInRange(confirmPassword, passwordRanges.min, passwordRanges.max)) {
+        confirmPasswordError = LoginSignUpConstants.passwordContainsSpecialCharacters;
+        isConfirmPasswordInErrorState = true; 
+    }
+
+    if (password !== confirmPassword) {
+        passwordError = LoginSignUpConstants.passwordsDoNotMatch;
+        isPasswordInErrorState = true;
+        confirmPasswordError = LoginSignUpConstants.passwordsDoNotMatch;
+        isConfirmPasswordInErrorState = true;
+    }
+
     return {
-        passwordError: "",
-        isPasswordInErrorState: false,
-        confirmPasswordError: "",
-        isConfirmPasswordInErrorState: false
+        passwordError: passwordError,
+        isPasswordInErrorState: isPasswordInErrorState,
+        confirmPasswordError: confirmPasswordError,
+        isConfirmPasswordInErrorState: isConfirmPasswordInErrorState
     };
 
 }
