7bffc02a9e8af4786de24544ded8e0bfa97c52b9
Jack Pettit <jpettit0317@gmail.com>
2023-04-14 09:22:46 -0500

    Added SignUpTestIds. This is used in testing when trying to get elements from the page.

---
+++ frontend/jobapptrackerfrontend/src/components/signup/SignUpTestIds.tsx
@@ -0,0 +1,15 @@
+export class SignUpTestIds {
+    static readonly signUpHeader = "signUpHeader";
+    static readonly floatingEmail = "floatingEmail";
+    static readonly emailField = "emailField";
+    static readonly floatingFirstname = "floatingFirstname";
+    static readonly firstNameField = "firstNameField";
+    static readonly floatingLastname = "floatingLastname";
+    static readonly lastnameField = "lastNameField";
+    static readonly floatingPassword = "floatingpassword";
+    static readonly passwordField = "passwordField";
+    static readonly floatingConfirmPassowrdField = "floatingConfirmPasswordField";
+    static readonly confirmPasswordField = "confirmPasswordField";
+    static readonly loginLink = "loginLink";
+    static readonly submit = "Submit";
+}
