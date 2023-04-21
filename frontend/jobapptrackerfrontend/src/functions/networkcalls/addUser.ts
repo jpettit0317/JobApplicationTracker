e61b4b1b711e0299ca50af5dcf987b4dfc2b00f5
Jack Pettit <jpettit0317@gmail.com>
2023-04-21 09:59:25 -0500

    Created an init function for HttpResponse in addUser.

--- frontend/jobapptrackerfrontend/src/functions/networkcalls/addUser.ts
+++ frontend/jobapptrackerfrontend/src/functions/networkcalls/addUser.ts
@@ -1,24 +1,36 @@
-import { error } from "console";
-import { HttpResponse, HttpResponseBuilder } from "../../model/httpresponses/HttpResponse";
+import { HttpResponse } from "../../model/httpresponses/HttpResponse";
 import { SignUp } from "../../model/interfaces/signup/SignUp";
 import axios from "axios";
+import { HttpStatusCodes } from "../../enums/HttpStatusCodes_enum";
+import { HttpResponseBuilder } from "../../model/builders/HttpResponseBuilder";
 
 export const addUser = async (signUp: SignUp, url: string): Promise<HttpResponse<string>> => {
-    return new Promise((resolve, reject) => {
-        axios.post<string>(url, signUp).then((res) => {
-          resolve(
-            new HttpResponseBuilder<string>(res.data)
-                .setErrorMessage("")
-                .setStatusCode(res.status)
-                .build()
-          );
-        })
-        .catch((err) => {
-          console.log("Fetch Error!!!", err);
-          return new HttpResponseBuilder<string>("")
-            .setErrorMessage(err.message)
-            .setStatusCode(404)
-            .build();
-        });
-    }); 
+  try {
+    const resp = await axios.post<string>(url, signUp);
+    console.log("Response data is ", resp.data);
+    
+    const addUserResponse = {
+      data: resp.data,
+      statusCode: resp.status,
+      errorMessage: ""
+    };
+
+    return createResponse(addUserResponse);
+
+  } catch (error: any) {
+    const errorString = error.message as string;
+
+    return new HttpResponseBuilder<string>("")
+      .setStatusCode(HttpStatusCodes.notFound)
+      .setErrorMessage(errorString)
+      .build();
+  }
+}
+
+export const createResponse = (resp: {data: string, statusCode: number, errorMessage: string} 
+  = {data: "", statusCode: 0, errorMessage: ""}): HttpResponse<string> => {
+    return new HttpResponseBuilder<string>(resp.data)
+      .setErrorMessage(resp.errorMessage)
+      .setStatusCode(resp.statusCode)
+      .build();
 }
