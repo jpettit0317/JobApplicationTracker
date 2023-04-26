8cc27ae07596516a9b0993531499481d60d71f50
Jack Pettit <jpettit0317@gmail.com>
2023-04-25 19:02:23 -0500

    Added close button and and header to SignUpAlert.

--- frontend/jobapptrackerfrontend/src/components/alerts/SignUpAlert.tsx
+++ frontend/jobapptrackerfrontend/src/components/alerts/SignUpAlert.tsx
@@ -5,12 +5,20 @@ import { SignUpAlertTestIds } from "../../enums/SignUpAlertTestIds_enum";
 
 export const SignUpAlert = (props: SignUpAlertProps) => {
     const alertMessage = props.alertMessage;
-
+    const header = "Error"
     return (
         <div>
             { props.shouldShow &&
-                <Alert variant="warning" onClose={props.closeButtonPressed} data-testid={SignUpAlertTestIds.signUpAlert}>
-                    <Alert.Heading data-testid={SignUpAlertTestIds.signUpAlertHeading}>{alertMessage}</Alert.Heading>
+                <Alert variant="warning" onClose={props.closeButtonPressed}
+                    data-testid={SignUpAlertTestIds.signUpAlert}
+                    dismissible
+                >
+                    <Alert.Heading data-testid={SignUpAlertTestIds.signUpAlertHeading}>
+                        {header}                
+                    </Alert.Heading>
+                    <p data-testid={SignUpAlertTestIds.signUpAlertBody}>
+                        {alertMessage}
+                    </p>
                 </Alert>
             }
         </div>
