6d627a1e9937ea9b9098d514b426caf5ea80ac7a
Jack Pettit <jpettit0317@gmail.com>
2023-05-06 10:40:51 -0500

    Refactored AuthenticationControllerTest.

--- backend/jobapplicationbackend/src/test/java/com/jpettit/jobapplicationbackend/unittests/controllers/AuthenticationControllerTest.java
+++ backend/jobapplicationbackend/src/test/java/com/jpettit/jobapplicationbackend/unittests/controllers/AuthenticationControllerTest.java
@@ -1,50 +1,95 @@
 package com.jpettit.jobapplicationbackend.unittests.controllers;
 
 import com.jpettit.jobapplicationbackend.controllers.AuthenticationController;
-import com.jpettit.jobapplicationbackend.models.requests.AuthenticationRequest;
+import com.jpettit.jobapplicationbackend.exceptions.UserExistsException;
+import com.jpettit.jobapplicationbackend.helpers.helper.helperpair.HelperPair;
+import com.jpettit.jobapplicationbackend.helpers.helpervars.AuthenticationControllerTestVars;
+import com.jpettit.jobapplicationbackend.models.requests.RegisterRequest;
+import com.jpettit.jobapplicationbackend.models.responses.AuthenticationResponse;
 import com.jpettit.jobapplicationbackend.services.AuthenticationService;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import com.jpettit.jobapplicationbackend.helpers.helper.AuthenticationControllerTestHelper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.mockito.Mockito.when;
 
-import static org.mockito.BDDMockito.given;
-
-@AutoConfigureJsonTesters
-@WebMvcTest(AuthenticationController.class)
+@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 class AuthenticationControllerTest {
-    @Autowired
-    private MockMvc mvc;
-
     @Mock
     private AuthenticationService authenticationService;
 
     @InjectMocks
     private AuthenticationController sut;
 
+    private RegisterRequest registerRequest;
 
+    private HelperPair<ResponseEntity<AuthenticationResponse>> pair;
+
+    private void setRegisterRequest(RegisterRequest newRequest) {
+        registerRequest = newRequest;
+    }
+
+    private void setActual(ResponseEntity<AuthenticationResponse> actual) {
+        pair.setActual(actual);
+    }
+
+    private void setExpected(ResponseEntity<AuthenticationResponse> expected) {
+        pair.setExpected(expected);
+    }
     @BeforeEach
     void setUp() {
+        setRegisterRequest(RegisterRequest.builder().build());
+        pair = HelperPair
+                .<ResponseEntity<AuthenticationResponse>>builder().build();
     }
 
     @AfterEach
     void tearDown() {
+        setRegisterRequest(null);
+        pair = null;
     }
 
     @Test
-    public void testWhenLoginHasNoUsernameOrNoPassword_controllerShouldReturn403() {
+    public void test_whenAuthServiceThrowsAnUserExistsException_ThenControllerShouldReturnForbidden()
+            throws Exception {
+        setRegisterRequest(AuthenticationControllerTestVars.alreadyExistingUserRequest);
+        setExpected(new ResponseEntity<>(AuthenticationControllerTestVars.
+                        alreadyExistingUserResponse, HttpStatus.OK));
+
+        final String userExistsErrorMessage = AuthenticationControllerTestVars
+                .createEmailErrorMessage(registerRequest.getEmail());
+        final UserExistsException userEx = AuthenticationControllerTestVars.createUserExistsException(userExistsErrorMessage);
+
+        when(authenticationService.register(ArgumentMatchers.any()))
+                .thenThrow(userEx);
+
+        setActual(sut.register(registerRequest));
+
+        AuthenticationControllerTestHelper.assertHttpResponseIsEqual(pair);
     }
 
-    private final AuthenticationRequest createResponse(String email, String password) {
-        return AuthenticationRequest.
-                builder()
-                .email(email)
-                .password(password)
-                .build();
+    @Test
+    public void register_WhenPassedInANonExistentUser_ShouldReturnToken() throws UserExistsException {
+        setRegisterRequest(AuthenticationControllerTestVars.validRegisterRequest);
+        setExpected(AuthenticationControllerTestVars.createResponseEntity(
+                AuthenticationControllerTestVars.validAuthenicationResponse, HttpStatus.OK));
+
+
+        when(authenticationService.register(ArgumentMatchers.any()))
+                .thenReturn(pair.getExpected().getBody());
+
+        setActual(sut.register(registerRequest));
+
+        AuthenticationControllerTestHelper.assertHttpResponseIsEqual(pair);
     }
 }
