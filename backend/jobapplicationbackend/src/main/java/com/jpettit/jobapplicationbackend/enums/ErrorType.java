2808b05a98215f0c67450776f845f05a67207aa9
Jack Pettit <jpettit0317@gmail.com>
2023-06-20 17:04:57 -0500

    Added another error Invalid input.

--- backend/jobapplicationbackend/src/main/java/com/jpettit/jobapplicationbackend/enums/ErrorType.java
+++ backend/jobapplicationbackend/src/main/java/com/jpettit/jobapplicationbackend/enums/ErrorType.java
@@ -4,4 +4,5 @@ public class ErrorType {
     public static final String USER_EXISTS = "USER_EXISTS";
     public static final String OTHER = "OTHER";
     public static final String NONE = "NONE";
+    public static final String INVALID_INPUT = "INVALID_INPUT";
 }
