7727e9de4eea69e1fa84f82c7d8401dc2cdd4058
Jack Pettit <jpettit0317@gmail.com>
2023-10-17 15:40:47 -0500

    Added ErrorRoute.

--- frontend/jobapptrackerfrontend/src/App.tsx
+++ frontend/jobapptrackerfrontend/src/App.tsx
@@ -12,36 +12,44 @@ import { JobAppListPage } from './components/jobapplist/JobAppListPage';
 import { AddJobAppPage } from './components/addJobApp/AddJobAppPage';
 import { ViewJobAppPage } from './components/viewJobApp/ViewJobAppPage';
 import { EditJobAppPage } from './components/editjobApp/EditJobAppPage';
+import { ErrorRoute } from './components/errorroute/ErrorRoute';
  
 const App = () => {
   const router = createBrowserRouter([
     {
       path: RoutePath.home,
-      element: <LoginPage />
+      element: <LoginPage />,
+      errorElement: <ErrorRoute />
     },
     {
       path: RoutePath.login,
-      element: <LoginPage />
+      element: <LoginPage />,
+      errorElement: <ErrorRoute />
     },
     {
       path: RoutePath.signup,
-      element: <SignUpPage />
+      element: <SignUpPage />,
+      errorElement: <ErrorRoute />
     },
     {
       path: RoutePath.jobapplist,
-      element: <JobAppListPage />
+      element: <JobAppListPage />,
+      errorElement: <ErrorRoute />
     },
     {
       path: RoutePath.addJobApp,
-      element: <AddJobAppPage />
+      element: <AddJobAppPage />,
+      errorElement: <ErrorRoute />
     },
     {
       path: RoutePath.viewJobAppPath,
-      element: <ViewJobAppPage id=""/> 
+      element: <ViewJobAppPage id=""/>,
+      errorElement: <ErrorRoute /> 
     },
     {
       path: RoutePath.editJobAppPath,
-      element: <EditJobAppPage />
+      element: <EditJobAppPage />,
+      errorElement: <ErrorRoute />
     }
   ]);
 
